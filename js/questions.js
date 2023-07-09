0759f12a5acb2367cbfdf2d3309ca79a5874c983
Jamshidbek2008 <jamshidbek00789@gmail.com>
2023-07-09 21:39:27 +0500

    viktarino

---
+++ js/questions.js
@@ -0,0 +1,79 @@
+let questions = [
+    {
+        numb: 1,
+        question: "Ozbekistonning poytaxti?",
+        answer: "Toshkent",
+        options: [
+            "Baku",
+            "Toshkent",
+            "Buxarest",
+            "Madrid"
+        ]
+    },
+    {
+        numb: 2,
+        question: "textni nima bilan beradi htmlda.",
+        answer: "p",
+        options: [
+            "p",
+            "m",
+            "matn",
+            "bilamdim"
+        ]
+    },
+    {
+        numb: 3,
+        question: "divni tez yozish uchun qanday yoziladi?",
+        answer: "div",
+        options: [
+            ".div",
+            "div.div",
+            ".div.ol",
+            "div"
+        ]
+    },
+    {
+        numb: 4,
+        question: "JavaScript fayl HTMLda qanday ulanadi?",
+        answer: "script",
+        options: [
+            "link",
+            "p",
+            "script",
+            "div"
+        ]
+    },
+    {
+        numb: 5,
+        question: "# belgisi qanaqa selektor ekanligini bildiradi?",
+        answer: "id",
+        options: [
+            "html",
+            "tag",
+            "class",
+            "id"
+        ]
+    },
+    {
+        numb: 6,
+        question: ". -belgisi qayerda ishlatildi",
+        answer: "class yozishda",
+        options: [
+            "html",
+            "class yozishda",
+            "id yozishda",
+            "scssda"
+        ]
+    },
+    {
+        numb: 7,
+        question: "inglischasiga salom nima?",
+        answer: "hello",
+        options: [
+            "qanaqasiyo",
+            "bonjour",
+            "hello",
+            "salom"
+        ]
+    },
+];
